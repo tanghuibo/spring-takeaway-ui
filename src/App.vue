32bee953aeef1648aaee440ea54990afb2540fd7
tanghuibo <1215893264@QQ.COM>
2019-01-24 02:43:40 +0800

    搭建基础框架，完成菜单功能

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,61 @@
 <template>
   <div id="app">
-    <el-row>
+    <el-row style="margin-right: 10px">
       <el-col :span="3">
         <div class="left">
-          <div class="left-head">
-            <div class="left-title">
-              <span>takeaway</span>
-            </div>
+          <div class="left-head" @click="goHome">
+            <TextHoverEffect class="mallki-txt" text="spring-takeaway" />
           </div>
           <div>
-            <el-menu default-active="1-4-1" class="el-menu-vertical-demo" :collapse="isCollapse">
-              <el-submenu index="1">
-                <template slot="title">
-                  <i class="el-icon-location"></i>
-                  <span slot="title">导航一</span>
-                </template>
-                <el-menu-item-group>
-                  <span slot="title">分组一</span>
-                  <el-menu-item index="1-1">选项1</el-menu-item>
-                  <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                  <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                  <span slot="title">选项4</span>
-                  <el-menu-item index="1-4-1">选项1</el-menu-item>
-                </el-submenu>
-              </el-submenu>
-              <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-              </el-menu-item>
-              <el-menu-item index="3" disabled>
-                <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
-              </el-menu-item>
-              <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-              </el-menu-item>
-            </el-menu>
+            <el-scrollbar wrap-class="scrollbar-wrapper">
+              <MenuView/>
+            </el-scrollbar>
           </div>
         </div>
+      </el-col>
+      <el-col :span="21">
+        <div class="right-head">1</div>
+        <GithubCorner url="https://github.com/tanghuibo/spring-takeaway-ui"/>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import menuData from "@/assets/menu.json";
+import GithubCorner from "@/components/GithubCorner/GithubCorner.vue";
+import TextHoverEffect from "@/components/TextHoverEffect/TextHoverEffect.vue";
+import MenuView from "@/components/MenuView/MenuView.vue";
 export default {
-  data() {
-    return {
-      isCollapse: false
-    };
+  components: {
+    GithubCorner,
+    TextHoverEffect,
+    MenuView
   },
-  mounted() {
-    console.log(menuData);
-  }
+  data() {
+    return {};
+  },
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    }
+  },
+  mounted() {}
 };
 </script>
 
 <style scoped lang = "scss">
-$head-height: 64px;
+$head-height: 6vh;
 .left {
   box-shadow: 2px 0 10px rgba(0, 0, 0, 0.1);
   height: 100vh;
   .left-head {
-    height: $head-height;
-    .left-title {
-      text-align: center;
+    text-align: center;
+    .mallki-txt {
       line-height: $head-height;
-      span {
-        padding: 2px 25%;
-        border-radius: 20px;
-        background-color: rgb(228, 245, 239);
-        font-weight: 100;
-        &:hover {
-          background-color: rgb(235, 248, 243);
-        }
-      }
     }
   }
+}
+.right-head {
+  line-height: $head-height;
 }
 </style>
---
+++ src/components/MenuView/MenuView.vue
@@ -0,0 +1,89 @@
+<template>
+  <el-menu
+    @select="selectMenu"
+    mode="vertical"
+    :default-active="activitiIndex"
+    :unique-opened="true"
+  >
+    <div v-for="menu in menuList" :key="menu.id">
+      <div v-if="menu.children != null && menu.children.length > 0">
+        <el-submenu :index="menu.id">
+          <template slot="title">
+            <i v-if="menu.icon" :class="menu.icon"></i>
+            <span>{{menu.title}}</span>
+          </template>
+          <el-menu-item
+            v-for="menuC in menu.children"
+            :key="menuC.id"
+            :index="menuC.id"
+          >{{menuC.title}}</el-menu-item>
+        </el-submenu>
+      </div>
+      <div v-else>
+        <el-menu-item :index="menu.id">
+          <i v-if="menu.icon" :class="menu.icon"></i>
+          <span slot="title">{{menu.title}}</span>
+        </el-menu-item>
+      </div>
+    </div>
+  </el-menu>
+</template>
+
+<script>
+import menuList from "@/assets/menu.json";
+export default {
+  data() {
+    return {
+      activitiIndex: null,
+      menuList: [],
+      menuIdMap: {},
+      menuUrlMap: {}
+    };
+  },
+  watch: {
+    $route(to) {
+
+      this.changeMenuActive(to.path);
+      
+    }
+  },
+  methods: {
+    selectMenu(index) {
+      this.$router.push(this.menuIdMap[index]);
+    },
+    changeMenuActive(path) {
+       this.activitiIndex = this.menuUrlMap[path];
+       console.log(this.activitiIndex);
+       
+    }
+  },
+  mounted() {
+    this.menuList = menuList;
+    let parentId = 1;
+    menuList.forEach(item => {
+      item.id = "" + parentId++;
+      if (item.url != null) {
+        this.menuIdMap[item.id] = item.url;
+        this.menuUrlMap[item.url] = item.id;
+      }
+      let childrenId = 1;
+      let children = item.children;
+      if (children != null) {
+        children.forEach(itemC => {
+          itemC.id = item.id + "-" + childrenId++;
+          if (itemC.url != null) {
+            this.menuIdMap[itemC.id] = itemC.url;
+            this.menuUrlMap[itemC.url] = itemC.id;
+          }
+        });
+      }
+    });
+
+    let path = this.$route.path;
+    this.changeMenuActive(path);
+  }
+};
+</script>
+
+<style scoped>
+</style>
